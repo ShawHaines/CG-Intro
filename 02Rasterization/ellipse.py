34314c78aae5a9d73e49142ae6b6adc334a0de14
Shaw Haines <haohaistone@qq.com>
2020-09-23 22:30:02 +0800

    The new python edition doesn't get accumulated errors.

--- 02Rasterization/ellipse.py
+++ 02Rasterization/ellipse.py
@@ -10,16 +10,17 @@ a,b,theta=[float(x) for x in input().split()]
 slices=100
 x=math.cos(theta)*a
 y=math.sin(theta)*a
+dphi=2*math.pi/slices
 r=np.array([x,y]).reshape((2,1))
 R=eulerMatrix(theta)
-A=np.array([[0,-a/b],[b/a,0]])
+A=np.array([[math.cos(dphi),-a/b*math.sin(dphi)],[b/a*math.sin(dphi),math.cos(dphi)]])
+# be careful with the transpose
 M=np.dot(R,np.dot(A,R.T))
 
 pointList=[]
-dphi=2*math.pi/slices
+
 for phi in np.linspace(0,2*math.pi,slices):
-    dr=np.dot(M,r)*dphi
-    r+=dr
+    r=np.dot(M,r)
     pointList.append(r)
     plt.scatter(*r)
 plt.show()
