8e946bf2b64a684e5e71d1985635bbac48a56b7c
Shaw Haines <haohaistone@qq.com>
2020-09-23 13:48:28 +0800

    Added ppm.cpp.

---
+++ 02Rasterization/ppm.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+void ppmRead(char* filename, unsigned char* data, int* w, int* h) {
+	char header[1024];
+	FILE* fp = NULL;
+	int line = 0;
+
+	fp = fopen(filename, "rb");
+	while (line < 2) {
+		fgets(header, 1024, fp);
+		if (header[0] != '#') {
+			++line;
+		}
+	}
+	sscanf(header, "%d %d\n", w, h);
+	fgets(header, 20, fp);
+	fread(data, (*w)*(*h) * 3, 1, fp);
+
+	fclose(fp);
+}
+void ppmWrite(const char* filename, unsigned char* data, int w, int h) {
+	FILE* fp;
+	fp = fopen(filename, "wb");
+
+	fprintf(fp, "P6\n%d %d\n255\n", w, h);
+	fwrite(data, w*h * 3, 1, fp);
+
+	fclose(fp);
+}
+
+int main() {
+	unsigned char data[400*300*3] = { 0 };
+	ppmWrite("test.ppm", data, 400, 300);
+	return 0;
+}
