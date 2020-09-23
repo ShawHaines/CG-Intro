c0902340d4268b56f7aac882ea6bc71c63aa0e21
Shaw Haines <haohaistone@qq.com>
2020-09-23 22:36:08 +0800

    Another version seems more straightforward.

--- 02Rasterization/ellipse.py
+++ 02Rasterization/ellipse.py
@@ -18,10 +18,19 @@ A=np.array([[math.cos(dphi),-a/b*math.sin(dphi)],[b/a*math.sin(dphi),math.cos(dp
 M=np.dot(R,np.dot(A,R.T))
 
 pointList=[]
-
+plt.scatter(*r)
 for phi in np.linspace(0,2*math.pi,slices):
     r=np.dot(M,r)
     pointList.append(r)
     plt.scatter(*r)
 plt.show()
 # print(M)
+
+# Another version:
+
+r=np.array([a,0]).reshape((2,1))
+plt.scatter(*np.dot(R,r))
+for phi in np.linspace(0,2*math.pi,slices):
+    r=np.dot(A,r)
+    plt.scatter(*np.dot(R,r))
+plt.show()
